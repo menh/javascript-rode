f68da7c497bdb7cf56941ba0c62f906d7ed34272
menhu <872204778@qq.com>
2019-07-10 18:14:06 +0800

    react

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -8,14 +8,56 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>欢迎来到菜鸟教程</h2>
+          <h2></h2>
         </div>
         <p className="App-intro">
-          你可以在 <code>src/App.js</code> 文件中修改。
+           <code></code> 文件中修改。
         </p>
       </div>
     );
   }
 }
 
+export class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+
+  tick(){
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <h1> hello clock </h1>
+        <h2> {this.state.date.toLocaleTimeString()}</h2>
+        <button onClick={handleClick}></button>
+      </div>
+    );
+  }
+}
+function handleClick(e) {
+    
+    console.log('链接被点击');
+  }
+export class Welcome extends Component{
+  render(){
+    return <h3>welcome </h3>;
+  }
+}
 export default App;
